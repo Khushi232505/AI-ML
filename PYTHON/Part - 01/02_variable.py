a951d10a1b1e1df04286daa09789aff1c259dacb
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-25 23:33:09 +0530

    Create 02_variable.py

---
+++ PYTHON/Part - 01/02_variable.py
@@ -0,0 +1,16 @@
+name = "Khushi"
+age = 20
+_pi = 3.14
+
+print(name)
+print(age)
+print(age-4)
+print("the value of pi is : ", _pi) 
+
+#name,age,pi - these all are identifiers 
+
+'''RULES for variable name/identifier:
+1- english letters 'a-z' and 'A-Z'
+2- can not start with digits(0-9)
+3- can start with underscore 
+4- name should be meaning ful'''
