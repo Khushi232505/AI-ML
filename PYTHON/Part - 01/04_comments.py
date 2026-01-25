6052de67a0adfe3b6699f42f363ec5a8e0625fb8
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-25 23:34:31 +0530

    Create 04_comments.py

---
+++ PYTHON/Part - 01/04_comments.py
@@ -0,0 +1,14 @@
+name = "Khushi"
+age = 20
+_pi = 3.14
+
+print(name)
+print(age)
+print(age-4)
+print("the value of pi is : ", _pi) 
+
+#this is comment which do not affect the output of the code it is only used for the better understanding of code
+
+'''This 
+is 
+a multiline comment'''
