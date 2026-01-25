e892f12a36365088ab106a2508385466e96d9b9b
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-25 23:33:54 +0530

    Create 03_datatypes.py

---
+++ PYTHON/Part - 01/03_datatypes.py
@@ -0,0 +1,18 @@
+name = "khushi"
+print(type(name))  # <class 'str'>
+
+age = 20
+print(type(age))   # <class 'int'>
+
+
+pi = 3.14
+print(type(pi))    # <class 'float'>
+
+
+num = 2
+isprime = True
+print(type(isprime))  # <class 'bool'>
+
+
+empty_var = None
+print(type(empty_var))  # <class 'NoneType'> 
