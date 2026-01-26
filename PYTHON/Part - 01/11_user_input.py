5d48ef54e9c4aadf46ce5eabf1db248524cca625
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-26 19:45:47 +0530

    Create 11_user_input.py

---
+++ PYTHON/Part - 01/11_user_input.py
@@ -0,0 +1,15 @@
+# a = input("enter a value: ")
+# print(a)
+
+# b= input("Enter your name: ")
+# print("welcome to our house", b)
+
+
+# when we use input function and take any value thorugh this input function then it takes that the value in the string form by default only
+# and whenever we add two strings together it is called concatination
+
+# sum of 2
+a = int(input("enter the value of a:"))
+b = int(input("enter the value of b:"))
+sum = a+b
+print(sum)
