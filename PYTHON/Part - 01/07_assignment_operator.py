fa245af49ce938512a67ba57dc73ebd6018ddc8d
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-26 19:41:57 +0530

    Create 07_assignment_operator.py

---
+++ PYTHON/Part - 01/07_assignment_operator.py
@@ -0,0 +1,23 @@
+a = 10
+b = a 
+print(b) # =
+ 
+# a = a+10
+a += 10
+print(a) # a = a+10
+
+
+a -= 5
+print(a) # a = a-5
+
+a *= 5
+print(a) # a = a*5
+
+a /= 5
+print(a) # a = a/5
+
+a %= 5
+print(a) # a = a%5
+
+a **= 5
+print(a) # a = a**5
