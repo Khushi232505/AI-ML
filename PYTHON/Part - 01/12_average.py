400390bb202df1a7a11eacb9be41de8b0b47e114
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-26 19:46:12 +0530

    Create 12_average.py

---
+++ PYTHON/Part - 01/12_average.py
@@ -0,0 +1,8 @@
+# WAP to print the average of two numbers
+
+a = float(input("enter the value of a : "))
+b = float(input("enter the value of b : "))
+
+avg = (a+b)/2
+
+print("The average of two numbers are: ", avg)
