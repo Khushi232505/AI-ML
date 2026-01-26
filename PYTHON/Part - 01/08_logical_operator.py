2c60945faa80ee751383388ac865eabd1cba9fb6
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-26 19:42:58 +0530

    Create 08_logical_operator.py

---
+++ PYTHON/Part - 01/08_logical_operator.py
@@ -0,0 +1,21 @@
+var = False
+
+print(not var)
+print(not (5 > 8)) 
+
+# Truth Table of "OR"
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth Table of "AND"
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+print (not (False))
+
+print((5 > 18) or (3>8))
+print((5 > 18) and (3>8))
