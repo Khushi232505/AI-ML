5ef669557b936562d01e4512bd15ed0c34727a5a
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-27 22:45:16 +0530

    Create 05_problem.py

---
+++ PYTHON/Part - 01/Assignment_1/05_problem.py
@@ -0,0 +1,14 @@
+x = 10 + 3 * 2 ** 2
+
+print("value of x is: ", x)
+
+'''Here the output is 22 because as we can see the expression that is given should be solved using operator precedence which means the operations are performed according to the priority level of the operator and the priority order is:
+    ()
+    **
+    *, /, %
+    +, -
+    ==, !=, >, >=, <, <=
+    not
+    and
+    or
+    =, +=, -='''
