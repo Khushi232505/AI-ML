d6a377e756f93462b3b73b2084abae8075ed9b5d
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-27 22:43:07 +0530

    Create 02_problem.py

---
+++ PYTHON/Part - 01/Assignment_1/02_problem.py
@@ -0,0 +1,13 @@
+# Take two numbers as input from the user and print their sum, difference, product, and quotient:
+
+a = float(input("Enter the value of a:"))
+b = float(input("Enter the value of b:"))
+
+sum = a+b
+diffrence = a-b
+product = a*b
+division = a/b
+print("sum:", sum)
+print("diffrence:", diffrence)
+print("product:", product)
+print("division:", division)
