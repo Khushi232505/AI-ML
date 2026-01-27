ea0ff42a1bd551f190923e9285b9ab39b5c0212b
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-27 22:43:29 +0530

    Create 03_problem.py

---
+++ PYTHON/Part - 01/Assignment_1/03_problem.py
@@ -0,0 +1,9 @@
+# Ask the user to enter two integers and one float.Convert them all to floats and print their average
+
+a = float(input("Enter first integer:"))
+b = float(input("Enter second integer:"))
+c = float(input("Enter a float value :"))
+
+avg = (a+b+c)/3
+
+print("Average of three numbers are: ", avg)
