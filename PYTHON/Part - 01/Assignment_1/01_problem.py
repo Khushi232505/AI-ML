17767ff701a515c0cf4f6d7c820efcaec17a9c2c
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-27 22:40:02 +0530

    Create 01_problem.py

---
+++ PYTHON/Part - 01/Assignment_1/01_problem.py
@@ -0,0 +1,6 @@
+# Write a program that asks the user for their name and age:
+
+name = input(("Enter the name: "))
+age = int(input("enter your age: "))
+
+print("Hello", name,",", " you are", age, "years old!")
