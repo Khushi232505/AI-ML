a861d2b89d2c5b9ebfddc8015ddfaf03dbc14660
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-28 23:52:43 +0530

    Create 09_problem.py

---
+++ PYTHON/Part - 01/Assignment_1/09_problem.py
@@ -0,0 +1,11 @@
+principal = input("enter the value of principal: ")
+rate = input("enter the rate: ")
+time = input("enter the time: ")
+
+P = float(principal)
+R = float(rate)
+T = float(time)
+
+SI = (P*R*T)/100
+
+print("simple interest: ", SI)
