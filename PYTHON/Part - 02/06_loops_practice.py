59bf7b9e402577e69ba5d9cc17fef0d7ac9b174e
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-30 21:59:22 +0530

    Create 06_loops_practice.py

---
+++ PYTHON/Part - 02/06_loops_practice.py
@@ -0,0 +1,41 @@
+# 1-  print number 1-5
+
+'''i = 1
+
+while(i<=5):
+    print(i)
+    i += 1'''
+
+
+# 2- print reverse number from 5-1
+'''i = 5
+
+while(i>=1):
+    print(i)
+    i -= 1 '''
+
+
+# 3- now iterartion starts from 0
+
+'''i = 0
+while(i<5):
+    print(i)
+    i += 1'''
+
+
+# 4- print multiplication table of any number 'n'
+
+'''n = int(input("enter number: "))
+i = 1
+while(i<=10):
+    print(n*i)
+    i += 1 
+    '''
+    
+#  if we start our code from 0 
+
+n = int(input("enter number: "))
+i = 0
+while(i<=10):
+    print(n*(i+1))
+    i += 1 
