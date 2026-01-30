764586b17b7f1636965c7bb1c95bf8517ea4ee77
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-30 21:58:48 +0530

    Create 05_while_loops.py

---
+++ PYTHON/Part - 02/05_while_loops.py
@@ -0,0 +1,14 @@
+'''infinte loop
+
+while True:
+    print("hello world")'''
+
+ #finite loop
+ 
+i = 1  # iterartor
+
+while (i <= 11):
+    print("hello world", i)
+    i += 1
+    
+print("after loop, count = ", i)
