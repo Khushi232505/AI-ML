42cf5e6e2d1312a010a6284a7e1b46b7a43cbf8f
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-31 21:01:20 +0530

    Create 11_range_function.py

---
+++ PYTHON/Part - 02/11_range_function.py
@@ -0,0 +1,22 @@
+# range() --> It is basically used to generate sequences
+# range(n)  --->  [0-(n-1)]
+'''range(start, stop, step)''' #step means updation
+
+''''In this the stop value is the compulsory one and the rest both start and step are optional and when there is no start snd step are given the there default value are: start-->0 and step--> +1.'''
+
+# here are some examples:
+'''range(5) --> 0,1,2,3,4
+range(1,6) --> 1,2,3,4,5
+range(1,10,2) --> 1,3,5,7,9'''
+
+
+
+# for i in range(6):
+#     print(i)
+    
+# for i in range(1,7):
+#     print(i)
+    
+    
+for i in range(1,10,2):
+    print(i)
