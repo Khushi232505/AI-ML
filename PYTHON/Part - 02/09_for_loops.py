e0c2fb050c84de8115bb84de6b3e2c9fa4ca704e
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-31 21:00:24 +0530

    Create 09_for_loops.py

---
+++ PYTHON/Part - 02/09_for_loops.py
@@ -0,0 +1,33 @@
+# for loops is used for sequential traversal 
+
+'''string = "khushi"
+
+if "h" in string:
+    print("h exist in string")
+
+# in => membership operator
+
+for var in string:
+    print(var) '''
+    
+    
+# range(n)  --->  [0-(n-1)]
+
+'''for i in range(5):
+    # print(i)
+    print("hiiii")'''
+    
+    
+# count the number of i's => 5
+
+word = "artificial intelligence"
+
+count = 0
+
+for ch in word:
+    if(ch == 'i'):
+        count += 1
+
+print("count of i : ", count)
+        
+
