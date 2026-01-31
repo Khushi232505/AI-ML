34e9b169fad7df2540b17948f5f6c8e18c2b346f
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-31 20:59:01 +0530

    Create 07_break.py

---
+++ PYTHON/Part - 02/07_break.py
@@ -0,0 +1,12 @@
+# break - it is used to terminate the loop
+
+i = 1
+
+while (i <= 10):
+    if(i % 6 == 0):
+        break
+    print(i)
+    i += 1 #updation
+    
+    
+print("outside loop now....")
