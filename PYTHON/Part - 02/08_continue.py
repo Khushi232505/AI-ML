9d9d9e470681d73ce19265c7564c5d356b6cb31f
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-31 20:59:36 +0530

    Create 08_continue.py

---
+++ PYTHON/Part - 02/08_continue.py
@@ -0,0 +1,21 @@
+'''continue - it is used  to skip the current iteration and move to the next iteration'''
+
+# i = 1
+# while(i <= 10):
+#     if(i % 4 == 0):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+    
+
+'''print odd numbers using continue '''
+i = 1
+while(i <= 10):
+    if(i % 2 == 0):
+        i += 1
+        continue
+    print(i)
+    i += 1
+        
+        
