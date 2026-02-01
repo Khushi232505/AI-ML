d276119d3f057cbbd2b99c91b346891edca05cc1
Khushi232505 <khushiitiwari23@gmail.com>
2026-02-01 21:53:11 +0530

    Create 15_function_types.py

---
+++ PYTHON/Part - 02/15_function_types.py
@@ -0,0 +1,12 @@
+# Types of function
+
+'''1 - built in function:
+    print()
+    input()
+    type()
+    range()'''
+    
+    
+'''2 - User defined function : which basically define ny the user in when we have to calll some function multiple times:
+    sum()
+    avg()...'''
