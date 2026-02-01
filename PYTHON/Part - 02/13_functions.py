196e5e9270cdb4a74c62ff8c63a8b551c830c53a
Khushi232505 <khushiitiwari23@gmail.com>
2026-02-01 21:40:06 +0530

    Create 13_functions.py

---
+++ PYTHON/Part - 02/13_functions.py
@@ -0,0 +1,25 @@
+# Functions are block of statements that perform the specific task and these are the reusable components of code:
+
+'''two parts:
+    1- definition --> core logic
+    2- call -->invoke(jo bhi logic likha hua hai usko execute karna)'''
+    
+    
+# def hello(): #function definition
+#     print("hello")
+#     print("from python")
+    
+# hello()# function call
+# hello()
+
+''' input values of function --> parameters
+    output values of function --> return '''
+    
+# function definition 
+def sum(a, b):
+    s = a + b
+    return s
+ 
+# function call
+ans = sum(3, 4)
+print(ans)
