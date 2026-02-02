a57785f62f1bbd48d893d840b881c666a7f418d2
Khushi232505 <khushiitiwari23@gmail.com>
2026-02-02 23:44:16 +0530

    Delete PYTHON/17_function_problem.py

--- PYTHON/17_function_problem.py
+++
@@ -1,9 +0,0 @@
-# WAP to print factorial of n:
-
-n = int(input("enter the number : "))
-
-fact = 1
-for i in range(1,n+1):
-    fact = fact * i
-    
-print("the factorial of the given numner is : ", fact)
