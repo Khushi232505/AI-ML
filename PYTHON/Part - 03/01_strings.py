7b0366b96dae439162b7c1369c1f477fa1a2ff93
Khushi232505 <khushiitiwari23@gmail.com>
2026-02-07 22:41:32 +0530

    Create 01_strings.py

---
+++ PYTHON/Part - 03/01_strings.py
@@ -0,0 +1,21 @@
+# Strings are immutable
+
+
+# Lenght of string
+
+word = "python"
+print(len(word))
+
+
+# Concatination
+
+word1 = "i love"
+word2 = " python"
+print(word1 + word2)
+
+
+word = "python"
+# print(word[2])
+
+for ch in word:
+    print(ch)
