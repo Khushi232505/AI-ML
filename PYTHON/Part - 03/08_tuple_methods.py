57ac03e3f27e0ee6d06cb4bdd7f1e9e40353c652
Khushi232505 <khushiitiwari23@gmail.com>
2026-02-08 23:52:37 +0530

    Create 08_tuple_methods.py

---
+++ PYTHON/Part - 03/08_tuple_methods.py
@@ -0,0 +1,20 @@
+tup = (1, 2, 3, 4, 3, 9)
+
+print(tup.count(3)) #  counts total occurences
+
+print(tup.index(4)) # returns first occurence index
+
+print(len(tup)) # tells us the lenght of the tuple
+
+# Concatination
+a = (1, 2, 4)
+b = (4, 5, 7)
+print(a+b)
+
+# Membership(in)
+print((3 in tup))
+print(5 in tup)
+
+# Repetition
+t = (3,) * 5
+print(t)
