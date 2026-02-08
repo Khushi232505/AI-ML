8093b690b9bb2907636db8543df63cd337c106a0
Khushi232505 <khushiitiwari23@gmail.com>
2026-02-08 23:51:46 +0530

    Create 07_tuples.py

---
+++ PYTHON/Part - 03/07_tuples.py
@@ -0,0 +1,13 @@
+# tuples are immutable.
+
+tup = (1, 2, 3, 4, 5)
+
+print(tup)
+print(len(tup))
+print(tup[2])
+print(tup[0:4])
+
+# the default value of starting string is 0 and the default value for the ending index is the lenght of the tuple
+
+tup_ = ("abc" , )
+print(type(tup_))
