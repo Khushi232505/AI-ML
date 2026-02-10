c6d91ddcca1e31cfb27576f867359c430c98bf7d
Khushi232505 <khushiitiwari23@gmail.com>
2026-02-10 23:38:39 +0530

    Create 12_sets.py

---
+++ PYTHON/Part - 03/12_sets.py
@@ -0,0 +1,17 @@
+'''sets --> collection of unique elements
+--> They are unordered , unindexed and also the elements in the sets are mutable but its elements are immutable.
+--> Sets can not have duplicate values.. '''
+
+s ={1, 2, 2, 2, 3}
+
+
+print(s)
+empty_set = set()
+print(type(empty_set))
+
+
+
+
+# s.add(8)
+# print(s)
+# print(len(s))
