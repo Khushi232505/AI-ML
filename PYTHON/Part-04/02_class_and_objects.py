c17330e16abb618f4f259b36f189afb62b7831c2
Khushi232505 <khushiitiwari23@gmail.com>
2026-02-15 21:32:41 +0530

    Create 02_class_and_objects.py

---
+++ PYTHON/Part-04/02_class_and_objects.py
@@ -0,0 +1,18 @@
+class Student:
+    subject = "python"
+    college = "BBDU"
+    year = "4th year "
+    
+# stu1 = Student()
+# stu2 = Student()
+# print(stu1.subject, stu1.college, stu1.year)
+# print(stu2.subject, stu2.college, stu2.year)
+
+stu1 = Student()
+print(type(stu1))
+
+# l = [1, 2]
+# print(type(l))
+
+s = set()
+print(type(s))
