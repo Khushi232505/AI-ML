3b80fe4618b324b1329a59c19433961e8b1a28c3
Khushi232505 <khushiitiwari23@gmail.com>
2026-03-28 23:53:06 +0530

    Create 01_functions.py

---
+++ PYTHON/Part-05/01_functions.py
@@ -0,0 +1,18 @@
+# function definition
+def calc_sum(a, b): # parameters
+
+    sum = a+b
+    print(sum)
+    return sum
+
+# function call
+calc_sum(8,8) # arguments
+calc_sum(5, 6 )
+
+
+def print_hello():
+    print("hello")
+
+# print_hello()
+output = print_hello()
+print(output)  #None
